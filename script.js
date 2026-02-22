e8595f37d6187b2973d6541f68bbd1b11f4cc1f0
nazrul23-human <nazrul23-human@gmail.com>
2026-02-23 04:18:45 +0600

    JS function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
- 
+ let InterviewList = [];
+ let RejectedList = []
+ let currentStatus = 'all'
+
+ let total = document.getElementById('total');
+ let InterviewCount = document.getElementById('InterviewCount');
+
+ const allFilterBtn = document.getElementById('allFilterBtn')
+ const InterviewFilterBtn = document.getElementById('InterviewFilterBtn')
+ const RejectedFilterBtn = document.getElementById('RejectedFilterBtn')
+
+ const allCardSection = document.getElementById('allCards');
+ const mainContainer = document.getElementById('main')
+ const filterSection = document.getElementById('filter-Section ')
+
